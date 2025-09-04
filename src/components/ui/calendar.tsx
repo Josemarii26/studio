7d9ec561ac5b8d69fb7cc4bfd3b339b43f435762
Jose M García Quijada <josemarigquijada@gmail.com>
2025-09-04 22:08:27 +0000

    perfecto!, cuando tengo seleccionado un dia me sale un cuadrado verde qu

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -37,14 +37,14 @@ function Calendar({
         head_cell:
           "flex-1 text-muted-foreground rounded-md font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "flex-1 text-center text-sm p-1 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "flex-1 text-center text-sm p-1 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-14 w-full p-0 font-normal aria-selected:opacity-100"
+          "h-12 w-12 p-0 font-normal aria-selected:opacity-100"
         ),
         day_range_end: "day-range-end",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+          "opacity-100",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
           "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
