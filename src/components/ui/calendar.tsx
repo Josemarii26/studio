7bc190e610f92d8766aee1e8e8fd3ffa91657b2f
Jose M García Quijada <josemarigquijada@gmail.com>
2025-09-07 09:45:58 +0000

    okey el siguiente cambio es en el calendario del dashboard, por alguna r

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -35,7 +35,7 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "flex-1 text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] flex-1",
         row: "flex w-full mt-2",
         cell: "flex-1 h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
