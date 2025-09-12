023549729e6e6545b47352c70d014059d56ec529
Jose M García Quijada <josemarigquijada@gmail.com>
2025-09-12 12:20:12 +0000

    me da este error en la build 2:19:21 PM: Netlify Build

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
       {
-        protocol: "httpshttps",
+        protocol: "https",
         hostname: "www.dropbox.com",
         port: "",
         pathname: "/**",
