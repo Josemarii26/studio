2549e2f3b80273180b173b204e5ba63c707bc977
Jose M García Quijada <josemarigquijada@gmail.com>
2025-09-12 15:26:53 +0000

    me sigue saliendo este error  Uncaught SyntaxError: Cannot use import st

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,25 +1,24 @@
-// Import the Firebase app and messaging libraries
-import { initializeApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging/sw';
+// Import Firebase libraries using importScripts
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js");
 
 // Your web app's Firebase configuration
 // This is HARDCODED because the service worker runs in the browser and doesn't have access to process.env
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 
-
 // Initialize the Firebase app in the service worker
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // Get an instance of Firebase Messaging in the service worker
-const messaging = getMessaging(app);
+const messaging = firebase.messaging();
 
 // Note: Background message handling would go here, but for now,
 // the primary purpose of this file is just to exist and be registered
